34d897bd2c2d644bbe55b35252c2c4bed4f5dad2
frankc60 <frankc60@gmail.com>
2020-12-08 18:58:44 +1300

    working

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# forexy
-NodeJS Forex results
+# Forexy
+
+Get the latest Forex Currency results, in a no dependency light weight fast package.
+
+## Install
+
+```js
+npm install forexy -s
+```
+
+## Use
+
+## Promise and Events
+
+Forexy returns a promise, which you can use in an async/await function, or using then().catch().
+
+Forexy also has a host of Events that you can use through the lifecycle process.
+
+Check out the examples to see various options used.
+
+### Events
+
+#### on request
+
+#### statusCode
+
+#### stream
+
+#### fulldata
+
+#### pair
+
+#### timestamp
+
+This returns a Date object, which can be further manipulated, this will be approximately the current timestamp.
+
+## Examples
--- index.js
+++ index.js
@@ -1,4 +1,157 @@
-//https://www.freeforexapi.com/api/live?pairs=gbpnzd
+const https = require("https");
 
-// hello
-sdsdsd;
+class Ee {
+  constructor() {
+    this.handlers = [];
+  }
+  on = (type, fn) => {
+    this.handlers.push([type, fn]);
+  };
+  emit = (type, data) => {
+    this.handlers
+      .filter((handler) => handler[0] == type)
+      .forEach((handler) => handler[1](data, type));
+  };
+}
+
+class Forexy extends Ee {
+  static #url = "https://www.freeforexapi.com/api/live";
+  #data2 = "hello";
+  constructor(params) {
+    super();
+    this.data = params;
+    this.mockData = false;
+  }
+  get(pairs) {
+    const uPairs = pairs.toUpperCase();
+    return new Promise((resolve, reject) => {
+      try {
+        this.emit("request", uPairs);
+        resolve(this._retrieveData(uPairs));
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  _retrieveData(d) {
+    return new Promise((resolve, reject) => {
+      if (this.mockData) {
+        this.emit("received", "header status>43334,etc");
+        this.emit("fulldata", [
+          { pair: "usbgbp" },
+          { price: 1.223 },
+          { date: 1222212112 },
+        ]);
+        resolve(1.223);
+      } else {
+        https
+          .get(`${Forexy.#url}?pairs=${d}`, (res) => {
+            let data = "";
+            this.emit("statusCode", res.statusCode);
+            this.emit("headers", res.headers);
+
+            res.on("data", (chunk) => {
+              //process.stdout.write(chunk);
+              this.emit("stream", chunk);
+              data += chunk;
+            });
+            res.on("end", () => {
+              try {
+                let prs = JSON.parse(data);
+                this.emit("fulldata", prs);
+                this.emit("pair", [d]);
+
+                if (prs.message) {
+                  reject(
+                    `${prs.message} Supported pairs: ${prs.supportedPairs}`
+                  );
+                } else {
+                  this.emit(
+                    "timestamp",
+                    new Date(prs.rates[d].timestamp * 1000)
+                  );
+                  this.emit("rate", prs.rates[d].rate);
+                  resolve(prs.rates[d].rate);
+                }
+
+                // {"message":"The currency pair 'GBPNZD' was not recognised or supported"
+              } catch (er) {
+                reject(er);
+              }
+            });
+          })
+          .on("error", (e) => {
+            reject(e);
+          });
+      }
+    });
+  }
+}
+
+//--------------------------------------------------------------------------------------------
+let a = new Forexy({ options: "dd" });
+
+a.on("request", (x) => {
+  console.log("on request:" + x);
+});
+
+a.on("received", (x) => {
+  console.log("on received: " + x);
+});
+
+a.on("fulldata", (d) => {
+  console.log("on fulldata:" + JSON.stringify(d));
+});
+
+a.on("stream", (d) => {
+  console.log("on steam: " + d);
+});
+
+a.on("statusCode", (d) => {
+  console.log("on statusCode:" + d);
+});
+
+a.on("timestamp", (d) => {
+  console.log("on timestamp:" + d);
+});
+
+a.on("rate", (d) => {
+  console.log("on rate:" + d);
+});
+
+a.on("pair", (d) => {
+  console.log("on pair:" + d);
+});
+
+a.get("usdLsl")
+  .then((d) => {
+    console.log("gbpusd> " + d);
+  })
+  .catch((e) => {
+    console.error("error1:" + e);
+  });
+
+/* tring USDGBP (exists) get:
+  received all data:{"rates":{"USDGBP":{"rate":0.748885,"timestamp":1607405286}},"code":200}
+
+
+
+
+/* trying GBPNZD get: (doesn't have)
+received all data:{"message":"The currency pair 'GBPNZD' was not recognised or supported","supportedPairs":["AUDUSD","EURGBP","EURUSD","GBPUSD","NZDUSD","USDAED",
+"USDAFN","USDALL","USDAMD","USDANG","USDAOA","USDARS","USDATS","USDAUD","USDAWG","USDAZM","USDAZN","USDBAM","USDBBD","USDBDT","USDBEF","USDBGN","USDBHD","USDBIF",
+"USDBMD","USDBND","USDBOB","USDBRL","USDBSD","USDBTN","USDBWP","USDBYN","USDBYR","USDBZD","USDCAD","USDCDF","USDCHF","USDCLP","USDCNH","USDCNY","USDCOP","USDCRC",
+"USDCUC","USDCUP","USDCVE","USDCYP","USDCZK","USDDEM","USDDJF","USDDKK","USDDOP","USDDZD","USDEEK","USDEGP","USDERN","USDESP","USDETB","USDEUR","USDFIM","USDFJD",
+"USDFKP","USDFRF","USDGBP","USDGEL","USDGGP","USDGHC","USDGHS","USDGIP","USDGMD","USDGNF","USDGRD","USDGTQ","USDGYD","USDHKD","USDHNL","USDHRK","USDHTG","USDHUF",
+"USDIDR","USDIEP","USDILS","USDIMP","USDINR","USDIQD","USDIRR","USDISK","USDITL","USDJEP","USDJMD","USDJOD","USDJPY","USDKES","USDKGS","USDKHR","USDKMF","USDKPW",
+"USDKRW","USDKWD","USDKYD","USDKZT","USDLAK","USDLBP","USDLKR","USDLRD","USDLSL","USDLTL","USDLUF","USDLVL","USDLYD","USDMAD","USDMDL","USDMGA","USDMGF","USDMKD",
+"USDMMK","USDMNT","USDMOP","USDMRO","USDMRU","USDMTL","USDMUR","USDMVR","USDMWK","USDMXN","USDMYR","USDMZM","USDMZN","USDNAD","USDNGN","USDNIO","USDNLG","USDNOK",
+"USDNPR","USDNZD","USDOMR","USDPAB","USDPEN","USDPGK","USDPHP","USDPKR","USDPLN","USDPTE","USDPYG","USDQAR","USDROL","USDRON","USDRSD","USDRUB","USDRWF","USDSAR",
+"USDSBD","USDSCR","USDSDD","USDSDG","USDSEK","USDSGD","USDSHP","USDSIT","USDSKK","USDSLL","USDSOS","USDSPL","USDSRD","USDSRG","USDSTD","USDSTN","USDSVC","USDSYP",
+"USDSZL","USDTHB","USDTJS","USDTMM","USDTMT","USDTND","USDTOP","USDTRL","USDTRY","USDTTD","USDTVD","USDTWD","USDTZS","USDUAH","USDUGX","USDUSD","USDUYU","USDUZS",
+"USDVAL","USDVEB","USDVEF","USDVES","USDVND","USDVUV","USDWST","USDXAF","USDXAG","USDXAU","USDXBT","USDXCD","USDXDR","USDXOF","USDXPD","USDXPF","USDXPT","USDYER",
+"USDZAR","USDZMK","USDZMW","USDZWD"],"code":1002}
+
+
+*/
